940fa8f3f3cdcad3349a174a5235f2c21e56ae26
catherineskoch <109420300+catherineskoch@users.noreply.github.com>
2025-11-13 14:28:07 +0300

    Add files via upload

---
+++ test_data.py
@@ -0,0 +1,17 @@
+new_pet_data = {
+    "id": 100998,
+    "name": "Kitty",
+    "category": {"id": 1, "name": "Cats"},
+    "photoUrls": ["https://example.com/photo.jpg"],
+    "tags": [{"id": 1, "name": "tag1"}],
+    "status": "available"
+}
+
+updated_pet_data = {
+    "id": 100999,
+    "name": "KittyUpdated",
+    "category": {"id": 1, "name": "Cats"},
+    "photoUrls": ["https://example.com/photo_updated.jpg"],
+    "tags": [{"id": 1, "name": "tag1"}],
+    "status": "sold"
+}
